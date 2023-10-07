bbdbc2ca74af767e997488981d1d010fb574c661
naimul_basher <naimulbasher122@gmail.com>
2023-10-07 20:39:26 +0600

    naimul_basher

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 
-import { PersistGate } from "redux-persist/integration/react";
+import { PersistGate } from "redux - persist/integration/react";
 import { persistor } from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
